577131ef9194ebd066a8fd576088a940244e48bc
JOY <103836369+angryOil@users.noreply.github.com>
2023-11-17 21:25:00 +0900

    모델 도메인 추가

---
+++ internal/domain/vo/info.go
@@ -0,0 +1,11 @@
+package vo
+
+type Info struct {
+	Id            int
+	CafeId        int
+	BoardId       int
+	Writer        int
+	Content       string
+	CreatedAt     string
+	LastUpdatedAt string
+}
